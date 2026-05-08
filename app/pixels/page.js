b585aaa0405f95e916ca5c01b132e0900fbc0dae
florafades <157669665+florafades@users.noreply.github.com>
2026-05-07 20:06:08 -0400

    deleted graphics folder
    changed it to pixels, added filler content. i think having empty page.js files causes deploy failure

---
+++ app/pixels/page.js
@@ -0,0 +1,33 @@
+
+import ArtGrid from "../../components/ArtGrid";
+
+export default function Pixels() {
+  return (
+    <main>
+
+
+
+
+      <div className="content-div">
+        <section className="card">
+          <h4> ˚₊‧ ꒰ა art portfolio ໒꒱ ‧₊˚</h4>
+          <img src="/images/pixels/roseglitterwow.gif" />
+          <p>made with love</p>
+
+        </section>
+      </div>
+
+      <div className="content-div">
+
+        <section className="content">
+         <ArtGrid />
+
+        </section>
+      </div>
+
+
+    </main>
+
+
+  );
+}
